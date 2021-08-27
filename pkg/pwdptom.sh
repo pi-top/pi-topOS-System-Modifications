b049043fd21d305ce80672d251546cc97e12d580
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-08-27 13:25:22 +0100

    Bullseye release (#30)

--- pkg/pwdptom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export TEXTDOMAIN=pt-os-mods
-
-# shellcheck disable=SC1091
-. gettext.sh
-
-zenity --password --title "$(gettext "Password Required")"
--- tests/helpers/mock_variables.bash
+++
@@ -1,8 +0,0 @@
-RESOLV_CONF_HEAD_FILE="/tmp/resolv.conf.head.test"
-export RESOLV_CONF_HEAD_FILE
-
-NEW_ALSA_OUTPUT_BREADCRUMB="/tmp/.configuredDefaultAlsaOutput.test"
-export NEW_ALSA_OUTPUT_BREADCRUMB
-
-NEW_PULSEAUDIO_OUTPUT_BREADCRUMB="/tmp/.configuredDefaultPulseAudioOutput.test"
-export NEW_PULSEAUDIO_OUTPUT_BREADCRUMB
